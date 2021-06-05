e78deefcbfaecbc321a4a0b574b7e4b4c7d283d1
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-06-05 07:18:59 +0000

    [finish]3

---
+++ leet.rb
@@ -0,0 +1,29 @@
+def convert_chart(chara)
+  case chara
+    when "A" then
+      "4"
+    when "E" then
+      "3"
+    when "G" then
+      "6"
+    when "I" then
+      "1"
+    when "O" then
+      "0"
+    else
+      chara
+  end
+end
+
+input_line = gets
+strings = input_line.to_s.split('') 
+
+
+  
+convert = []
+strings.each do | chara |
+  convert.push(convert_chart(chara))
+end
+  
+output = convert.join
+puts output
